0c85337c4d6ffa2fd284446c6e48ee85a77b24dc
JUSTIN KABANZA <JUSTIN KABANZA@KABANZA>
2019-03-04 13:11:45 +0200

    Ajout des listes

--- training/src/java/com/crip/Universite.java
+++ training/src/java/com/crip/Universite.java
@@ -7,7 +7,10 @@ package com.crip;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.json.simple.parser.ParseException;
 
 /**
@@ -24,8 +27,37 @@ public class Universite {
     public void setNomUniv(String nomUniv) {
         this.nomUniv = nomUniv;
     }
+    
+    public Universite(){
+        
+    }
+    public Universite(String nomUniv){
+       this.nomUniv= nomUniv;
+    }
     public void Enregistrer(Universite univ)throws ClassNotFoundException, SQLException, IOException, FileNotFoundException, ParseException{
         DBConnection cnx=new DBConnection();
         cnx.Execute_Query("Insert into Universite (nomUniv) values ('"+ univ.nomUniv +"')");
+    }
+    
+    public List<Universite> listeUniversite() throws ClassNotFoundException, SQLException, IOException, FileNotFoundException, ParseException{
+        List<Universite> lst = new ArrayList<>();
+        
+            DBConnection conn = new DBConnection();
+            ResultSet result = conn.Data_Source(
+                    "SELECT "
+                            + "nomUniv "                    
+                            + "FROM Universite"
+            );
+            
+            while(result.next())
+            {
+                lst.add(new Universite(
+                        result.getString("nomUniv")
+                       
+                        )
+                );
+            }
+        
+        return lst;
     }
 }
