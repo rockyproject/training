e4011e3ccf8be7a53782717d1cc36e75be2c7612
JUSTIN KABANZA <JUSTIN KABANZA@KABANZA>
2019-03-12 19:11:33 +0200

    Ajout des formateurs avec des modules

---
+++ training/src/java/com/crip/Evaluation.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crip;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author JUSTIN KABANZA
+ */
+@ManagedBean
+@SessionScoped
+public class Evaluation {
+
+    /**
+     * Creates a new instance of Evaluation
+     */
+    public Evaluation() {
+    }
+    
+}
---
+++ training/src/java/com/crip/Formation.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crip;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author JUSTIN KABANZA
+ */
+@ManagedBean
+@SessionScoped
+public class Formation {
+
+    /**
+     * Creates a new instance of Formation
+     */
+    public Formation() {
+    }
+    
+}
